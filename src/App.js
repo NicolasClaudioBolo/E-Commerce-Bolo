b0b9066b076b2b32cf9723b345e61a65d2901454
NicoBolo <nicolascbolo@gmail.com>
2022-02-02 18:57:08 -0300

    Contador con botones en App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  constructor(){
+      super();
+      this.state = {
+      counter: 0,
+  };
+
+} 
+
+    plusBtn = () => {
+      this.setState({ counter: this.state.counter +1 });
+    }
+
+    minusBtn = () => {
+      this.setState({ counter: this.state.counter -1 });
+    }
+
+    render(){
+    return (
+    <div className="CounterApp">
+        <h1>Counter: {this.state.counter}</h1>
+        <button onClick={this.minusBtn}>-</button>
+        <button onClick={this.plusBtn}>+</button>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
