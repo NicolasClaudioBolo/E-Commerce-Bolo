1df220d0555eb052319f2da378b062c0c4bc1e28
NicoBolo <nicolascbolo@gmail.com>
2022-03-20 16:19:49 -0300

    Último commit

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -4,40 +4,32 @@ import axios from 'axios';
 import {Spinner} from 'reactstrap';
 import '../Spinner.css';
 import { useParams } from 'react-router-dom';
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, FieldPath } from "firebase/firestore";
 import { db } from '../../firebase/firebaseConfig';
 
 const ItemDetailContainer = () => {
 
-    // LLAMADO A LA API
     const {id} = useParams()
 
     const [product, setProduct] = useState({})
     const [isLoading, setIsLoading] = useState(true);
 
-    // axios llama a la API y mapea los productos por id
-
     useEffect(()=>{
       const getItems = async ()=>{
-        const q = query(collection(db, "fakestoreapi"));
+        const q = query(collection(db, 'fakestoreapi'), where('__name__', '==', id));
+        console.log('id', id)
         const docs = [];
         const querySnapshot = await getDocs(q)
         querySnapshot.forEach((doc)=>{
+          console.log('doc', doc)
             docs.push({...doc.data(), id: doc.id})
         })
-        console.log(docs)
-
-        // TODO linea a modificar
+        console.log('docs', docs)
+        console.log('querySnapshot', querySnapshot)
         setProduct(docs[0]);
         setIsLoading(false);
     }
-    console.log(product)
-    getItems();
-        // axios('https://fakestoreapi.com/products')
-        // .then((res) => setProduct(res.data.find((product) => product.id == id)));
-        // setTimeout(() => {
-        //   setIsLoading(false);
-        // }, 1500);       
+    getItems();      
     }, [id]);
 
   return (
